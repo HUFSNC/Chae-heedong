fcff6ec5eae176ffa1489bc4463c8abeedc19bf2
채희동 <60247020+Chae-heedong@users.noreply.github.com>
2024-08-15 21:35:34 +0900

    Update index.js
    사소한 오류 수정

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let button = document.getElementById("button");
 button.onclick = function() {
     let x = document.getElementById("x");
     let y = document.getElementById("y");
-
+    x=parseInt(x)
+    y=parseInt(y)
     let sum = x.value + y.value;
 
     document.getElementById("result").value = sum;
