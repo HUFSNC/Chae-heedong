500fd79351b32189d00ef4d5408e1ba1014d7142
채희동 <60247020+Chae-heedong@users.noreply.github.com>
2024-08-15 21:39:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ let button = document.getElementById("button");
 button.onclick = function() {
     let x = document.getElementById("x");
     let y = document.getElementById("y");
-    x=parseInt(x)
-    y=parseInt(y)
-    let sum = x.value + y.value;
+    x=parseInt(x);
+    y=parseInt(y);
+    let sum = x+y;
 
     document.getElementById("result").value = sum;
 };
